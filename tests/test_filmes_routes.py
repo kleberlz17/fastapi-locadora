9acc5fe293c91c8fe778a9fafe6ace06deb84ad1
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2025-07-28 23:38:15 -0300

    testes de integração | filmes routes

---
+++ tests/test_filmes_routes.py
@@ -0,0 +1,13 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database import Base, get_db
+from app.main import app
+from tests.test_cliente_routes import TestingSessionLocal
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
