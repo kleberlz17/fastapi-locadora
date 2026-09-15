7b895a77f8da0e3f399e14f54855476c60fb454b
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2026-09-14 21:33:20 -0300

    Containerizando aplicação para hospedagem na AWS

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/fastapi_locadora"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@localhost:5432/fastapi-locadora"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
